1ff369f90a1156244b1ba72628dd6bbf913fb71b
rmsmsgood <rmsmsgood@naver.com>
2021-03-19 14:09:06 +0900

    backup

--- simulation.jl
+++ simulation.jl
@@ -1,131 +1,155 @@
-cd(@__DIR__) # 파일 저장 경로
-
 @time using Random
+@time using Statistics
 @time using Base.Threads
 @time using Plots
 
-function duel(A::Int, B::Int; p=0.8)
-  if A == 0
-    return B
-  elseif B == 0
-    return A
-  end
+cd(@__DIR__)
+# if pwd() == "c:\\Users\\rmsms\\OneDrive\\lab\\RPS"
+#     cd("210312") # 파일 저장 경로
+# end
+println(pwd())
 
-  extraA = mod(A-1, 3) + 1
-  extraB = mod(B-1, 3) + 1
-
-  if extraA == 1 && extraB == 2
-    return B
-    # if rand() < 0.2 return A else return B end
-  elseif extraA == 2 && extraB == 3
-    return B
-    # if rand() < 0.2 return A else return B end
-  elseif extraA == 3 && extraB == 1
-    return B
-    # if rand() < 0.2 return A else return B end
-  end
-
-  intraA = A ≤ 3 ? 1 : 2
-  intraB = B ≤ 3 ? 1 : 2
-
-  if intraA == 1 && intraB == 1
-    # return A
-    if rand() < (1-p) return 0 else return A end
-  elseif intraA == 2 && intraB == 2
-    # return 0
-    if rand() < p return 0 else return A end
-  elseif intraA == 2 && intraB == 1
-    # return A
-    if rand() < (1-p) return B else return A end
-  elseif intraA == 1 && intraB == 2
-    # return B
-    if rand() < p return B else return A end
-  end
+function action(center::Char, right::Char)
+    probability = rand()
+    if probability < σ
+        if center == 'A' && right == 'B'
+            return ('A', '∅')
+        elseif center == 'B' && right == 'C'
+            return ('B', '∅')
+        elseif center == 'C' && right == 'A'
+            return ('C', '∅')
+        else
+            return (center, right)
+        end
+    elseif (1-μ) < probability
+        if center == 'A' && right == '∅'
+            return ('A', 'A')
+        elseif center == 'B' && right == '∅'
+            return ('B', 'B')
+        elseif center == 'C' && right == '∅'
+            return ('C', 'C')
+        else
+            return (center, right)
+        end
+    else
+        return (right, center)
+    end
 end
 
 # 0:W: Empty
 # 1:R:R: Rock
 # 2:B:P: Paper
 # 3:G:S: scissors
+# colormap_RPS = [
+#   colorant"#FFCCCC",
+#   colorant"#99FF66",
+#   colorant"#99CCFF",
+#   colorant"#FFFFFF"]
 colormap_RPS = [
-  colorant"#FFFFFF",
-  colorant"#FFCCCC",
-  colorant"#99CCFF",
-  colorant"#CCFFCC",
-  colorant"#F66666",
-  colorant"#6699FF",
-  colorant"#339900"]
+  colorant"#FF3333",
+  colorant"#33CC00",
+  colorant"#0066FF",
+  colorant"#FFCC00",
+  colorant"#9933CC",
+  colorant"#FFFFFF"]
 
-row_size = column_size = 200
-endtime = 3000
-stage_lattice = zeros(Int64, row_size, column_size)
+  
+N = row_size = column_size = 100 + 4
+endtime = 1000
+
+log10M =(-(6:-1:3))
+
+for ϵ ∈ 2(10. .^ log10M)*(row_size-4)*(column_size-4)
+# for ϵ ∈ 5:5
+
+stage_lattice = Array{Char, 2}(undef, row_size, column_size)
+stage_lattice .= '∅'
+
+σ = 1
+μ = 1
+ε = ϵ
+# ε = 10
+Σ = (σ + μ + ε)
+σ = σ / Σ
+μ = μ / Σ
+ε = ε / Σ
 
 x = Int64[]
 y = Int64[]
 z = Int64[]
-X = Int64[]
-Y = Int64[]
-Z = Int64[]
 
 Random.seed!(0);
-for k in 1:18
-  stage_lattice[rand(2:(row_size-1)), rand(2:(column_size-1))] = mod(k-1,6) + 1
-end
+# for t ∈ 1:2
+#     I = rand(3:(row_size - 1)); J = rand(2:(column_size - 1))
+#     stage_lattice[I, J] = 1
+#     I = rand(3:(row_size - 1)); J = rand(2:(column_size - 1))
+#     stage_lattice[I, J] = 2
+#     I = rand(3:(row_size - 1)); J = rand(2:(column_size - 1))
+#     stage_lattice[I, J] = 3
+#     # I = rand(3:(row_size - 1)); J = rand(2:(column_size - 1))
+#     # stage_lattice[I, J] = 4
+#     # I = rand(3:(row_size - 1)); J = rand(2:(column_size - 1))
+#     # stage_lattice[I, J] = 5
+# end
 
+stage_lattice[3:(row_size - 2), 3:(column_size - 2)] =
+ (rand(['A','B','C'], row_size - 4, column_size -4))
+
+stage_lattice[1, 1] = 'A'
+stage_lattice[1, 2] = 'B'
+stage_lattice[1, 3] = 'C'
+
+# for t = 1:endtime
 lattice = @animate for t = 1:endtime
-  print("|")
-  if mod(t, 100) == 0 println() end
-  # @threads
-  stage_duel = copy(stage_lattice)
-  @threads for j in 2:(row_size-1)
-    for i in 2:(row_size-1)
-      I = i
-      J = j
-      A = stage_lattice[I, J]
+    print("|")
+    if mod(t, 100) == 0 println(t) end
+    # stage_duel = copy(stage_lattice)
+    
+    # for (i,j) ∈ shuffle([(i,j) for i in 3:(row_size - 2) for j in 3:(column_size - 2)])
+    for τ ∈ 1:(N^2)
+        j = rand(3:(row_size - 2))
+        i = rand(3:(column_size - 2))
+        나 = stage_lattice[i,j]
 
-      I += rand([-1,0,1])
-      J += rand([-1,0,1])
-      # if rand(Bool)
-      #   I += rand([-1,1])
-      # else
-      #   J += rand([-1,1])
-      # end
-      B = stage_lattice[I, J]
-  
-      stage_duel[i,j] = duel(A, B)
+        if rand([true, false])
+            Δx = rand([-1,1])
+            Δy = 0
+        else
+            Δx = 0
+            Δy = rand([-1,1])
+        end
+        # 가 = stage_lattice[i + Δx, j + Δy]
+        다 = stage_lattice[i + Δx, j + Δy]
+
+        stage_lattice[i,j], stage_lattice[i + Δx, j + Δy] = action(나, 다)
+        #     catch
+        #         println("A: $A, B: $B")
+        # # println("A: $(typeof(A)), B: $(typeof(A))")
+        # # println(typeof(duel(A, B, coop_A, coop_B)))
+        #     end
     end
-  end
-  stage_lattice = stage_duel
-  push!(x, sum(stage_lattice .== 1))
-  push!(y, sum(stage_lattice .== 2))
-  push!(z, sum(stage_lattice .== 3))
-  push!(X, sum(stage_lattice .== 4))
-  push!(Y, sum(stage_lattice .== 5))
-  push!(Z, sum(stage_lattice .== 6))
+    # stage_lattice = stage_duel
+    # stage_lattice[3:(row_size - 1), 2:(column_size - 1)] .*= (rand(row_size - 3, column_size -2) .< 0.9)
+    push!(x, sum(stage_lattice .== 'A'))
+    push!(y, sum(stage_lattice .== 'B'))
+    push!(z, sum(stage_lattice .== 'C'))
+    # push!(X, sum(stage_lattice .== 4))
+    # push!(Y, sum(stage_lattice .== 5))
+    # push!(Z, sum(stage_lattice .== 6))
 
-  stage_lattice = stage_duel
-  figure = heatmap(stage_lattice, color=colormap_RPS,
-    xaxis=false,yaxis=false,axis=nothing, size = [400,400], legend = false)
-  end
-gif(lattice, "lattice.mp4", fps = 24)
-
-time_evolution = plot()
-plot!(time_evolution, x, linecolor = colormap_RPS[2], label = "x")
-plot!(time_evolution, y, linecolor = colormap_RPS[3], label = "y")
-plot!(time_evolution, z, linecolor = colormap_RPS[4], label = "z")
-plot!(time_evolution, X, linecolor = colormap_RPS[5], label = "X")
-plot!(time_evolution, Y, linecolor = colormap_RPS[6], label = "Y")
-plot!(time_evolution, Z, linecolor = colormap_RPS[7], label = "Z")
-png(time_evolution, "time evolution.png")
-
-time_evolution_sum = plot()
-plot!(time_evolution_sum, X+x, linecolor = colormap_RPS[2], label = "X+x")
-plot!(time_evolution_sum, Y+y, linecolor = colormap_RPS[3], label = "Y+y")
-plot!(time_evolution_sum, Z+z, linecolor = colormap_RPS[4], label = "Z+z")
-png(time_evolution_sum, "time evolution sum.png")
-
-traj = @animate for t in 1:10:(endtime-10)
-  plot(X[1:t],-Y[1:t],Z[1:t], camera = (45,45), linealpha = (1:t)/t)
-  xlims!(0,maximum(X)); ylims!(-maximum(Y),0); zlims!(0,maximum(Z))
+    # stage_lattice = stage_duel
+    figure = heatmap(stage_lattice, color=colormap_RPS,
+    xaxis=false,yaxis=false,axis=nothing, size=[400, 400], legend=false)
 end
-gif(traj, "traj.mp4", fps = 12)
+gif(lattice, "result_lattice $ϵ.mp4", fps=24)
+
+time_evolution = plot(legend=:topleft)
+plot!(time_evolution, x, linecolor=colormap_RPS[1], label="x")
+plot!(time_evolution, y, linecolor=colormap_RPS[2], label="y")
+plot!(time_evolution, z, linecolor=colormap_RPS[3], label="z")
+# plot!(time_evolution, X, linecolor=colormap_RPS[5], label="X")
+# plot!(time_evolution, Y, linecolor=colormap_RPS[6], label="Y")
+# plot!(time_evolution, Z, linecolor=colormap_RPS[7], label="Z")
+png(time_evolution, "result_time evolution $ϵ.png")
+
+end
